10211cddf493deb0160ebc8ba084c0869029a497
jainvinay001 <35107468+jainvinay001@users.noreply.github.com>
2021-02-18 09:08:05 +0530

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -6,15 +6,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-dirname="/home/ec2-user/express-app"
-if [[ ! -d "$dirname" ]]
-then
-    if [ -L $dirname]
-then
-    echo "File doesn't exist. Creating now"
-    mkdir ./$dirname
-    echo "File created"
-    else
-        echo "File exists"
-    fi
+DIR="/home/ec2-user/express-app"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
