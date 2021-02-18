5a18bf2390d57e0f8ff9a105ab0ec9295be51c05
jainvinay001 <35107468+jainvinay001@users.noreply.github.com>
2021-02-18 07:55:53 +0530

    Update before_install.sh

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,4 +1,3 @@
-chmod 777
 #!/bin/bash
 
 #download node and npm
@@ -7,10 +6,15 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-DIR=chmod 777 "/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
+dirname="/home/ec2-user/express-app"
+if [[ ! -d "$dirname" ]]
+then
+    if [ -L $dirname]
+then
+    echo "File doesn't exist. Creating now"
+    mkdir ./$dirname
+    echo "File created"
+    else
+        echo "File exists"
+    fi
 fi
